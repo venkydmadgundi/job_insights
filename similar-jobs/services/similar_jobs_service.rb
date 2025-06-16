5a0ccea177d716992213403b02d825d248efb3a7
Vyankatesh Madgundi <35259008+venkydmadgundi@users.noreply.github.com>
2025-06-16 14:49:08 +0530

    Update similar_jobs_service.rb

--- similar-jobs/services/similar_jobs_service.rb
+++ similar-jobs/services/similar_jobs_service.rb
@@ -5,13 +5,13 @@ class SimilarJobsService
   end
 
   def find_similar(limit: 5)
-    Rails.cache.fetch(cache_key, expires_in: 1.hour) do
+    # Rails.cache.fetch(cache_key, expires_in: 1.hour) do
       base_scope = Job.where.not(id: @job.id)
       base_scope = base_scope.with_similar_title(@job.title) if @options[:title]
       base_scope = base_scope.with_tags(@job.tag_ids) if @options[:tags]
       base_scope = base_scope.in_industry(@job.industry_id) if @options[:industry]
       base_scope.limit(limit)
-    end
+    # end
   end
 
   private
